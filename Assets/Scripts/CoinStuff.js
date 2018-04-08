0ece5ac66001aa4af8462bf9151c464401dfb6b6
SteeltownRiot <steelerswiz@gmail.com>
2018-04-07 20:31:42 -0400

    Initial commit
    This is the initial commit of an existing project

---
+++ Assets/Scripts/CoinStuff.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+public var points : int = 5;
+
+function OnTrigger2D(Collider2D other) 
+{
+    Debug.Log("Triggered! Worth " + points + " points!")
+
+    Destroy(gameObject)
+}
